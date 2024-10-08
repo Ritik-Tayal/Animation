0e370513baea09eca28bbdad5003cddd401d1aec
Ritik-Tayal <ritik23153049@akgec.ac.in>
2024-10-08 15:14:06 +0530

    Random opacity

--- script.js
+++ script.js
@@ -18,20 +18,22 @@ let c=canvas.getContext("2d");
 //     c.stroke();
 // }
 
-function bubble(x,y,radius,dx,dy){
+function bubble(x,y,radius,dx,dy,color,opac){
     this.x=x;
     this.y=y;
     this.radius=radius;
     this.dx=dx;
     this.dy=dy;
+    this.color=this.color;
+    this.opac=opac;
 
     this.draw=function() {
-
+        // z=Math.random();
         c.beginPath();
         c.arc(this.x,this.y,this.radius,0,Math.PI*2,false);
-        c.strokeStyle="blue";
+        c.strokeStyle="white";
         c.stroke();
-        c.fillStyle="rgba(0,0,255,0.3)";
+        c.fillStyle=`rgba(0,0,${color},${opac})`;
         c.fill();
 
     }
@@ -60,7 +62,9 @@ for( let i=0;i<200;i++){
     let radius=Math.random()*30;
     let dx=(Math.random()-0.5)*4;
     let dy=(Math.random()-0.5)*4;
-    bubbles.push(new bubble(x,y,radius,dx,dy));
+    let color=Math.floor((Math.random()*50)+205);
+    let opac=((Math.random()*0.4)+0.2);
+    bubbles.push(new bubble(x,y,radius,dx,dy,color,opac));
 }
 
 function animate(){
